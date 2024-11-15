ec0dd562c1ab9e4d470231c455aec04a51f7d77d
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2024-11-15 15:25:33 +0530

    login page test

--- tests/Feature/Livewire/Pages/LoginPageTest.php
+++ tests/Feature/Livewire/Pages/LoginPageTest.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace Tests\Feature\Livewire\Pages;
+
+use App\Livewire\Auth\LoginPage;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class LoginPageTest extends TestCase
+{
+    /** @test */
+    public function renders_successfully()
+    {
+        Livewire::test(LoginPage::class)
+            ->assertStatus(200);
+    }
+}
