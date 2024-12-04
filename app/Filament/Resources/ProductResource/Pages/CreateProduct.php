fe1671caf95e0385eb4b06508ad5f6b66709cb93
chamathishanka <pkchamath2000@gmail.com>
2024-12-05 00:02:43 +0530

    CreateProduct and EditProduct update

--- app/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -8,5 +8,6 @@ use Filament\Resources\Pages\CreateRecord;
 
 class CreateProduct extends CreateRecord
 {
+    // Specifies the resource class that this page is associated with
     protected static string $resource = ProductResource::class;
 }
